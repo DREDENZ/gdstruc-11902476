689a470c66b51aea5aa8221cebe73d267fe85bf7
Dwight Terence Tablizo <dwightterence.tablizo@benilde.edu.ph>
2021-08-23 13:54:08 +0800

    Added Sample Project

---
+++ Sample Project/src/com/dwightterencetablizo/Main.java
@@ -0,0 +1,22 @@
+package com.dwightterencetablizo;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        // Prints out "Hello World"
+        //System.out.println("Hello World");
+
+        // Scanner class -> gets user input
+        Scanner scanner = new Scanner(System.in);
+
+        // Output
+        System.out.println("What is your name? ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + name);
+    }
+}
